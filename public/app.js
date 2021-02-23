6c315a891d8777aa241f5254d38b435d0d07cdaa
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-02-23 17:51:19 +0100

    Spectre, not Milligram

--- public/app.js
+++ public/app.js
@@ -6,8 +6,9 @@ const App = (() => {
   useMeta({name: 'author', content: 'Jarek Zgoda'});
 
   return (
-    <div class="container">
+    <div class="container grid-md">
       <h1>Hello, WMR!</h1>
+      <p>It's been loong time!</p>
     </div>
   )
 });
